7ce3bf6c7b5248e3d06d99d044c8c133b283eebe
Kweku Nartey Ablakwa <lordskyishere1@gmail.com>
2026-07-30 18:01:42 +0000

    godot game

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 
-
-func _on_body_entered(body: Node2D) -> void:
+#Collides with player on contact
+func _on_body_entered(_body: Node2D) -> void:
 	print("+1 Coin")
 	queue_free()
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,10 +5,10 @@ extends Area2D
 func _on_body_entered(body: Node2D) -> void:
 	print("You Died!")
 	
-	#engine.time_scale adds a slow mo effect
+	#Handle game speed
 	Engine.time_scale = 0.5
+	#Removes collision from player
 	body.get_node("CollisionShape2D").queue_free()
-	#queue free removes the collision
 	timer.start()
 	
 
